8c4f574c9aacbba8d7eacfd4c13730d500d35775
Michael Rivera <soymrmike@gmail.com>
2024-04-18 19:11:34 -0700

    Got seq map working.

--- plot.scala
+++ plot.scala
@@ -3,8 +3,9 @@ import scala.math
 val expr1 = (n: Int) => 5 * (math.log10(n) / math.log10(2))
 val expr2 = (n: Int) => math.log10(50000 * n) / math.log10(2)
 
+
 @main
-def Main(): Array[Array[Double]] = {
+def Main(): Seq[Array[Double]] = {
   println("|----------|----------|")
   /*
   val output: Array[Array[Double]] = Seq(10,12,14,16,18,20).map {
@@ -13,30 +14,14 @@ def Main(): Array[Array[Double]] = {
   */
 
   
-  val output: Seq[Double] = Seq(10,12,14,16,18,20).map {
-    // val v: Array[Double] = Array(expr1(_))
-    // val result: Array[Double] = Array(expr1(_));
-    // println(result(0));
-    expr1(_)
-  }
-  /*
-  val output: Array[Array[Double]] = Seq(10,12,14,16,18,20).map {
-    val v1 = expr1(_)
-    val v2 = expr2(_)
-    (v1, v2)    
-  }
-  */
-  
-  for(line <- output) {
-    println(line)
-  }
-
-
-  val ret: Array[Array[Double]] = Array();  
-  return ret;
+  val output: Seq[Array[Double]] = Seq(10,12,14,16,18,20).map(n => {
+    Array(expr1(n), expr2(n))
+  })
+  return output
 }
 
-def output(output: Array[Array[Double]]) = {
+
+def output(output: Seq[Array[Double]]) = {
   for(line <- output) {
     val v1 = line(0)
     val v2 = line(1)
