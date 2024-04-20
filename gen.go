3551a41e11ccbcfe10aa50c2cab91f9ffc3f766c
Michael Rivera <soymrmike@gmail.com>
2024-04-20 04:54:57 -0700

    Graph of exercise 3.3-5a.

--- gen.go
+++ gen.go
@@ -6,19 +6,21 @@ import (
   "encoding/json"
   "io/ioutil"
   "github.com/dougwatson/Go/v3/math/factorial"
+  "fmt"
+  "strconv"
 )
 
 type TwoLines struct {
-  X []int       `json:"x"` 
-  Y []float64    `json:"y"`
+  X []int64       `json:"x"` 
+  Y []float64     `json:"y"`
   Y2 []float64   `json:"y2"`
 }
 
 type ThreeLines struct {
-  X []int       `json:"x"` 
-  Y []float64    `json:"y"`
-  Y2 []float64   `json:"y2"`
-  Y3 []float64   `json:"y3"`
+  X []int64       `json:"x"` 
+  Y []float64     `json:"y"`
+  Y2 []float64    `json:"y2"`
+  Y3 []float64    `json:"y3"`
 }
 
 func roundFloat(val float64, precision uint) float64 {
@@ -26,14 +28,23 @@ func roundFloat(val float64, precision uint) float64 {
 	return math.Round(val*ratio) / ratio
 }
 
+func sciNotation(input float64) string {
+  count := 0
+  for input >= 10 {
+    count += 1
+    input = input / 10.0;
+  }
+  return strconv.FormatFloat(input, 'e', -1, 64) + "e" + strconv.Itoa(count);
+}
+
 func exercise3p3d4c() *ThreeLines {
-  expr1 := func(n int) float64 {
+  expr1 := func(n int64) float64 {
     return 5 * math.Log2(float64(n))
   }
-  expr2 := func(n int) float64 {
+  expr2 := func(n int64) float64 {
     return math.Log2(50000.0 * float64(n))
   }
-  expr3 := func(n int) float64 {
+  expr3 := func(n int64) float64 {
     inputN := n + 10;
     if(inputN <= 0) {
       return 0;
@@ -42,7 +53,7 @@ func exercise3p3d4c() *ThreeLines {
   }
   
   result := &ThreeLines{}
-  for n := 1; n <= 40; n += 1 {
+  for n := int64(1); n <= 40; n += 1 {
     v1 := expr1(n)
     v2 := expr2(n)
     v3 := expr3(n)
@@ -56,17 +67,17 @@ func exercise3p3d4c() *ThreeLines {
 }
 
 func exercise3p3d5b() *TwoLines {
-  expr1 := func(n int) float64 {
+  expr1 := func(n int64) float64 {
     return roundFloat(math.Pow(float64(n), 3), 3);
   }
   
-  expr2 := func(n int) float64 {
+  expr2 := func(n int64) float64 {
     factArg := int(math.Ceil(math.Log2(math.Log2(float64(n)))))
     return roundFloat(float64(factorial.Iterative(factArg)), 3)
   }
   
   result := &TwoLines{}
-  for n := 1; n <= 30; n++ {
+  for n := int64(1); n <= 30; n++ {
     v1 := expr1(n);
     v2 := expr2(n);
     result.X = append(result.X, n)
@@ -77,21 +88,65 @@ func exercise3p3d5b() *TwoLines {
   return result;
 }
 
+func exercise3p3d5a() *TwoLines {
+  expr1 := func(n float64) float64 {
+    factArg := int(math.Ceil(math.Log2(n)))
+    return roundFloat(float64(factorial.Iterative(factArg)), 3)
+  }
+  
+  expr2 := func(n float64) float64 {
+    return roundFloat(math.Pow(n, 2), 3);
+  }
+    
+  
+  result := &TwoLines{}
+  start := math.Pow(float64(2), 8)
+  end := math.Pow(float64(2), 12)
+  step := (end - start) / 10
+
+  for n := start; n <= end; n += step {
+    v1 := expr1(n);
+    v2 := expr2(n);
+    result.X = append(result.X, int64(n))
+    result.Y = append(result.Y, v1)
+    result.Y2 = append(result.Y2, v2)
+  }
+  
+  examined := []float64 { 
+    math.Pow(float64(2), 20),
+    math.Pow(float64(2), 21),
+    math.Pow(float64(2), 23),
+  } 
+  for _, n := range examined {
+    v1 := expr1(n)
+    v2 := expr2(n)
+    
+    
+    // s := fmt.Sprintf("|%s|%s|%s|", sciNotation(n), sciNotation(v1), sciNotation(v2))
+    s := fmt.Sprintf("|%15.2f|%15.f|%15.f|", n, v1, v2)
+    fmt.Println(s)
+  }
+  
+  return result;
+}
+
 func main() {
-  result := exercise3p3d5b();
+  result := exercise3p3d5a();
   
   output, err := json.Marshal(*result)
   if err != nil {
     log.Fatal(err)
   }
 
+  /*
   var outputPretty []byte
   outputPretty, err = json.MarshalIndent(*result, "", "  ")
   if err != nil {
     log.Fatal(err)
   }
+  */
   
   ioutil.WriteFile("data.json", output, 0644)
   log.Println("Wrote data.json")
-  println(string(outputPretty))
+  // println(string(outputPretty))
 }
--- input.js
+++ input.js
@@ -2,16 +2,40 @@
 import { Aristochart } from './aristochart/aristochart.es6.js';
 import { logging } from './logging.js';
 
-document.addEventListener("DOMContentLoaded", () => {    
+document.addEventListener("DOMContentLoaded", () => {
+  const labelRender = function(style, text, x, y, type, i){
+		if(i % this.options.label[type].step == 0) {
+			var label = style.label[type];
+			if(type == "x") y = y + (style.tick.major + label.offsetY)*this.resolution;
+			if(type == "y") x = x - (style.tick.major + label.offsetX)*this.resolution, y += label.offsetY*this.resolution;
+
+			this.ctx.font = label.fontStyle + " " + (label.fontSize*this.resolution) + "px " + label.font;
+			this.ctx.fillStyle = label.color;
+			this.ctx.textAlign = label.align;
+			this.ctx.textBaseline = label.baseline;
+
+      if(text > 10000) {
+  			var substr = /(\-?\d+(\.\d)?)/.exec(text) || [];
+  			this.ctx.fillText(text.toExponential(3), x, y);        
+      } else {
+  			var substr = /(\-?\d+(\.\d)?)/.exec(text) || [];
+  			this.ctx.fillText(substr[0], x, y);        
+      }
+    }
+	}
+  
   const plotContainer = document.getElementById('plot');
   fetch('/data.json')
   .then(r => r.json())
   .then((data) => {
     const plot = new Aristochart(plotContainer, {
+      label: {
+        render: labelRender
+      },
       style: {
         y: {
           line: {
-            stroke: 'blue'
+            stroke: '#298281'
           }
         },
         y2: {
@@ -21,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         y3: {
           line: {
-            stroke: 'dodgerblue'
+            stroke: 'blue'
           }
         },
         default: {
@@ -36,10 +60,17 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     	data: data
     });
+    const toImageButton = document.querySelector('.to-image');
+    toImageButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      const asImageContainer = document.querySelector('.as-image');
+      asImageContainer.appendChild(plot.toImage());
+    });
     // logging.info(JSON.stringify(plot.options.style.default, null, 2));
   })
   .catch(err => {
     logging.info("Error: " + err);
   });
+    
   // logging.info(JSON.stringify(a2.options, null, 2));
 });
