e9e72ee42c4f923e973203115ab0d90ea4ce7688
Michael Rivera <soymrmike@gmail.com>
2024-04-19 22:31:15 -0700

    go mod init. Outputing line coords to 2 decimal places.

--- gen.go
+++ gen.go
@@ -4,13 +4,18 @@ import (
   "math"
   "log"
   "encoding/json"
-  "fmt"
+  "io/ioutil"
 )
 
-type Line struct {
-  X []int      `json:"x"` 
-  Y []string   `json:"y"`
-  Y2 []string   `json:"y2"`
+type TwoLines struct {
+  X []int       `json:"x"` 
+  Y []float64    `json:"y"`
+  Y2 []float64   `json:"y2"`
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
 }
 
 func main() {
@@ -22,20 +27,21 @@ func main() {
     return math.Log2(50000.0 * float64(n))
   }
   
-  result := &Line{}
-  for n := 10; n <= 20; n += 2 {
+  result := &TwoLines{}
+  for n := 1; n <= 40; n += 1 {
     v1 := expr1(n)
     v2 := expr2(n)
     result.X = append(result.X, n);
-    result.Y = append(result.Y, fmt.Sprintf("%.2f", v2));
-    result.Y2 = append(result.Y2, fmt.Sprintf("%.2f", v1));
+    result.Y = append(result.Y, roundFloat(v2, 2));
+    result.Y2 = append(result.Y2, roundFloat(v1, 2));
   }
-
   
   output, err := json.Marshal(*result)
   if err != nil {
     log.Fatal(err)
   }
   
+  ioutil.WriteFile("data.json", output, 0644)
   println(string(output))
+  log.Println("Wrote data.json")
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mikedll/notebooks
+
+go 1.21.1
